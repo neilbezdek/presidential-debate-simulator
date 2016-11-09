9f3855f0b40efe02985d4a244482f0e023fea619
Neil Bezdek <neilbezdek@Neils-MacBook-Pro.local>
2016-11-08 18:39:25 -0700

    app is pretty AND working

--- flask_bootstrap/app.py
+++ flask_bootstrap/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 app = Flask(__name__)
+from document_similarity import answer_questions
 
 # home page
-@app.route('/')
+@app.route('/', methods = ['POST','GET'])
 def index():
-    return render_template('index.html', title='Hello!')
+    question = request.form.get('question')
+    if not question:
+        return render_template('index.html', question = None)
+    else:
+        answer = answer_questions(question)
+        return render_template('index.html', question = question, obama = ''.join([answer['OBAMA'][:200],'...']), trump = ''.join([answer['TRUMP'][:200],'...']), hrclinton = ''.join([answer['H. R. CLINTON'][:200],'...']), bclinton = ''.join([answer['B. CLINTON'][:200],'...']), kennedy = ''.join([answer['KENNEDY'][:200],'...']), reagan = ''.join([answer['REAGAN'][:200],'...']))
 
 # @app.route('/more/')
 # def more():
